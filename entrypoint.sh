2fd3ebd426f6b13611f997152637b48b8bc933af
ZSFan888 <zsfan888@hotmail.com>
2026-08-27 14:44:06 +0800

    Auto-detect proxy host/port from Railway built-in variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-PROXY_HOST="${PROXY_HOST:-CHANGE_ME.proxy.rlwy.net}"
-PROXY_PORT="${PROXY_PORT:-0}"
+PROXY_HOST="${PROXY_HOST:-${RAILWAY_TCP_PROXY_DOMAIN:-CHANGE_ME.proxy.rlwy.net}}"
+PROXY_PORT="${PROXY_PORT:-${RAILWAY_TCP_PROXY_PORT:-0}}"
 SECRET="ee184a82a5a62c20392baafa444cbafe997777772e62696e672e636f6d"
 LINK="https://t.me/proxy?server=${PROXY_HOST}&port=${PROXY_PORT}&secret=${SECRET}"
 QR="https://api.qrserver.com/v1/create-qr-code/?size=220x220&data=${LINK}"
